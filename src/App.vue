da09cfbe7dc4f99c50a57f80535b3d209ec236f8
wSjyGithub <982766818@qq.com>
2019-10-25 23:30:23 +0800

    one

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,37 @@
 <template>
   <div id="app">
-    
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+       <!-- 头部 -->
+       <mt-header fixed title="黑马程序员-vue项目"></mt-header>
+       sss
+       <nav class="mui-bar mui-bar-tab">
+        <a class="mui-tab-item mui-active" href="#tabbar">
+          <span class="mui-icon mui-icon-home"></span>
+          <span class="mui-tab-label">首页</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-chat">
+          <span class="mui-icon mui-icon-contact"></span>
+          <span class="mui-tab-label">会员</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-contact">
+          <span class="mui-icon  mui-icon-extra mui-icon-extra-cart"><span class="mui-badge">9</span></span>
+          <span class="mui-tab-label">购物车</span>
+        </a>
+        <a class="mui-tab-item" href="#tabbar-with-map">
+          <span class="mui-icon mui-icon-search"></span>
+          <span class="mui-tab-label">搜索</span>
+        </a>
+       
+      </nav>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    
   }
 } 
 </script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,10 @@
 import Vue from 'vue'
+import Mint from 'mint-ui';
+import 'mint-ui/lib/style.css';
+import '../dilist/mui/css/mui.min.css'
+import '../dilist/mui/css/icons-extra.css'
+Vue.use(Mint);
+
 import App from './App.vue'
 
 Vue.config.productionTip = false
@@ -6,3 +12,4 @@ Vue.config.productionTip = false
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+  
